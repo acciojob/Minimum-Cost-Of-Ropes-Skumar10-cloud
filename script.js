d66db63c1a997627ecfbd420c3d9cc05881d5292
Skumar10-cloud <120595419+Skumar10-cloud@users.noreply.github.com>
2023-02-27 21:27:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
   function minCost(event){
     event.preventDefault(); //use this stop the default behaviour which is page referesh auto
-    var input = document.querySelector('input').value;//.value always return into string
-    var arr = input.split(',');  //split all the elements and return it into the array
+    var inputElement = document.querySelector('input').value;//.value always return into string
+    var arr = inputElement.split(',');  //split all the elements and return it into the array
     
       arr.sort(function(a,b) {return a-b});// sorting the array
 //we need to access first two element
@@ -13,18 +13,18 @@
     while(arr.length>1){
       var res = Number(arr[0]) + Number(arr[1]);//write number to convert into number until it will be shown in a string
       arr.splice(0,2);
-	  
-      arr.push(res);
-	  cost += res;
-			//or 
+			  //or 
       //arr.shift(); //use this one to remove element from starting
 	  //arr.shift(); //want to remove 2 element thats why used twice
 
+      arr.push(res);
+	  cost += res;
+			
 	arr.sort(function(a,b) {return a-b});
       
     }
-    document.querySelector("div").textContent=cost;
+    // document.querySelector("div").textContent=cost;
 			//or can use below one also
-	// document.getElementById("result").textContent = cost;
+	document.getElementById("result").textContent = cost;
   }
   
