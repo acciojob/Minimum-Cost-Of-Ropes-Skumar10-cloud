0b57de66b064dc91c0f7fccba5e0c6520dd9508f
Skumar10-cloud <120595419+Skumar10-cloud@users.noreply.github.com>
2023-02-27 20:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
   function minCost(event){
-    event.preventDefault();
-    var input = document.querySelector("input").value;
-    var arr = input.split(",");
-    var cost = 0;
+    event.preventDefault(); //use this stop the default behaviour which is page referesh auto
+    var input = document.querySelector("input").value;//.value always return into string
+    var arr = input.split(",");  //split all the elements and return it into the array
+    
+      arr.sort(function(a,b) {return a-b});// sorting the array
+//we need to access first two element
+//add them & store in a variable res
+
+//add the res in array
+//increment cost by res
+	var cost = 0;
     while(arr.length>1){
-      arr.sort(function(a,b) {return a-b});
-      var temp = Number(arr[0]) + Number(arr[1]);
+      var temp = Number(arr[0]) + Number(arr[1]);//write number to convert into number until it will be shown in a string
       cost+=temp;
       arr.push(temp);
-      arr.splice(0,2);
-      arr.sort(function(a,b) {return a-b});
+      // arr.splice(0,2); 
+      arr.shift(); //use this one to remove element from starting
+	  arr.shift(); //want to remove 2 element thats why used twice
+
+	arr.sort(function(a,b) {return a-b});
       
     }
-    document.querySelector("div").textContent=cost;
+    // document.querySelector("div").textContent=cost;
+			//or can use below one also
+	document.getElementById("result").textContent = cost;
   }
   
